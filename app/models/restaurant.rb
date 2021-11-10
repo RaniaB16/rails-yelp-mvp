9a23a8d59106ef15bef167f1708994ecd795731b
Rania BOUTEKRABET <raniabtkt@gmail.com>
2021-11-10 16:40:53 +0100

    rails-yelp-mvp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   validates :name, presence: true
   validates :address, presence: true
-  validates :phone_number, presence: true
-  validates :category, presence: true
-  validates :category, acceptance: { accept: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :category, inclusion: { in: CATEGORIES }
+  has_many :reviews, dependent: :destroy
 end
